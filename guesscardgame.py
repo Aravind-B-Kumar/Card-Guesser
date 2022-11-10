8707c5c55b5836cc88fcf7203e14d27c474b35f1
Aravind-B-Kumar <116909631+Aravind-B-Kumar@users.noreply.github.com>
2022-11-10 19:48:48 +0530

    Update guesscardgame.py

--- guesscardgame.py
+++ guesscardgame.py
@@ -1,9 +1,45 @@
 import random
 import time
 
-print("\n\t\t\t   CARD GAME\n")
+def display(cards:list,limit:int=3, header:bool=True):
+    count=0
+    print("\n       ⬇COLUMN 1⬇","      ⬇COLUMN 2⬇","      ⬇COLUMN 3⬇\n") if header else ...
+    for i in cards: 
+        print("\t",f"{i:10}",end="")
+        count+=1
+        if count%limit==0:
+            print("\n")
+
+def distributer(cards:list):
+    return cards[::3],cards[1::3],cards[2::3]
+
+def sorter(cards:list):
+    column_a,column_b,column_c = distributer(cards)
+    select_column = column_selector()
+
+    if select_column == 1:
+        cards = column_c+column_a+column_b
     
-print("\t\tI WILL FIND THE CARD IN YOUR MIND\n\n")
+    elif select_column == 2: 
+        cards = column_a+column_b+column_c 
+    
+    elif select_column == 3: 
+        cards = column_a+column_c+column_b 
+    
+    return cards
+
+def column_selector():
+    while True:
+        select_column = input("\nWhich column has your card(1/2/3) ? : ")
+        if select_column.isdigit():
+            if int(select_column) in range(1,4):
+                return int(select_column)
+            else:
+                print("Input the column number that contains your card")
+        else:
+            print("Invalid input..")
+    
+print("\n\t\t\t   CARD GAME\n\t\tI WILL FIND THE CARD IN YOUR MIND\n\n")
 
 card_list = ["Ace(♣)" ,"2(♣)" ,"3(♣)" ,"4(♣)" ,"5(♣)" ,"6(♣)" ,"7(♣)" ,
             "8(♣)" ,"9(♣)" ,"10(♣)" ,"Jack(♣)" ,"Queen(♣)" ,"King(♣)" ,
@@ -22,27 +58,18 @@ while True:
     random_card = random.choice(card_list)
     if random_card not in card_list_choice:
         card_list_choice.append(random_card)
-
         if len(card_list_choice)==21:
             break
-
 
 card_list_display=[]
 while True:
     random_display = random.choice(card_list_choice)
     if random_display not in card_list_display:
         card_list_display.append(random_display)
-
         if len(card_list_display)==21:
             break
 
-
-b=0
-for i in card_list_display:
-    print(f"{i:10}",end="")
-    b=b+1
-    if b%7 == 0:
-        print("\n")
+display(card_list_display ,limit=7 , header= False)
 
 print("\nThink about anyone card from the above list and keep it in your mind")
 
@@ -51,7 +78,6 @@ time.sleep(1.5)
 while True:
     ready = input("Are you READY (y/n) ? : ")
     ready=ready.lower()
-        
     if ready == "y":
         break
     elif ready == "n":
@@ -59,122 +85,10 @@ while True:
     else :
         print("Invalid Move..")
 
-
-count=0
-print("\n       ⬇COLUMN 1⬇","      ⬇COLUMN 2⬇","      ⬇COLUMN 3⬇\n")
-for i in card_list_choice: 
-    print("\t",f"{i:10}",end="")
-    count+=1
-    if count%3==0:
-        print("\n")
-
-
-list_1 = []
-for i in card_list_choice:
-    list_1.append(i)
-
-
-column_a = list_1[::3]
-column_b = list_1[1::3]
-column_c = list_1[2::3]
-
-
-while True:
-    select_column = input("\nWhich column has your card(1/2/3) ? : ")
-
-    if select_column.isdigit():
-        if int(select_column) in range(1,4):
-            break
-        else:
-            print("Input the column number that contains your card")
-
-    else:
-        print("Invalid input..")
-
-
-#LIST 2
-list_2=[]
-if int(select_column) == 1:
-    list_2 = column_c+column_a+column_b
- 
-elif int(select_column) == 2: 
-    list_2 = column_a+column_b+column_c 
- 
-elif int(select_column) == 3: 
-    list_2 = column_a+column_c+column_b 
-
-
-count=0
-print("\n       ⬇COLUMN 1⬇","      ⬇COLUMN 2⬇","      ⬇COLUMN 3⬇\n")
-for i in list_2: 
-    print("\t",f"{i:10}",end="")
-    count+=1
-    if count%3==0:
-        print("\n")
-
-
-column_p = list_2[::3]
-column_q = list_2[1::3]
-column_r = list_2[2::3]
-
-while True:
-    select_column = input("\nWhich column has your card(1/2/3) ? : ")
-    if select_column.isdigit():
-        if int(select_column) in range(1,4):
-            break
-        else:
-            print("Input the column number that contains your card")
-    elif select_column.isalpha():
-        print("Invalid input..")
-
-
-list_3=[]
-if int(select_column) == 1:
-    list_3 = column_r+column_p+column_q
-
-elif int(select_column) == 2:
-    list_3 = column_p+column_q+column_r
-
-elif int(select_column) == 3:
-    list_3 = column_p+column_r+column_q
-
-
-count=0
-print("\n       ⬇COLUMN 1⬇","      ⬇COLUMN 2⬇","      ⬇COLUMN 3⬇\n")
-for i in list_3: 
-    print("\t",f"{i:10}",end="")
-    count+=1
-    if count%3==0:
-        print("\n")
-
-
-column_x = list_3[::3]
-column_y = list_3[1::3]
-column_z = list_3[2::3]
-    
-
-
-while True:
-    select_column = input("\nWhich column has your card(1/2/3) ? : ")
-
-    if select_column.isdigit():
-        if int(select_column) in range(1,4):
-            break
-        else:
-            print("Input the column number that contains your card")
-
-    else:
-        print("Invalid input..")
-
-
-list_4=[]
-if int(select_column) == 1:
-    list_4 = column_z+column_x+column_y
-
-elif int(select_column) == 2:
-    list_4 = column_x+column_y+column_z
-
-elif int(select_column) == 3:
-    list_4 = column_x+column_z+column_y
-
+display(card_list_choice)
+list_2=sorter(card_list_choice)
+display(list_2)
+list_3=sorter(list_2)
+display(list_3)
+list_4=sorter(list_3)
 print("The card in your mind is...",list_4[10],". HAHA\n")
